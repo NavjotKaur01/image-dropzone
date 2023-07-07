ff6f6821f31cbbc6acb2a5beb6f42f202c8265d7
Navjot Kaur <navjot.kaur@asiasoftwaresolutions.com>
2023-07-07 13:30:56 +0530

    Changes to show delete button with condition

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,107 +11,107 @@ interface Props {
   sendUploadImage?: (image: string, imageType: string) => void
   handleResetImage?: () => void
   handleChangeImage?: () => void
+  showDelete?: boolean
 }
-const ImageDropZone = forwardRef(({ from, sendUploadImage, handleResetImage, handleChangeImage }: Props, ref) => {
-  useImperativeHandle(ref, () => ({
-    handleSetPath(image: string | null, imageType: string | null) {
-      if (!image) {
+const ImageDropZone = forwardRef(
+  ({ from, sendUploadImage, handleResetImage, handleChangeImage, showDelete }: Props, ref) => {
+    useImperativeHandle(ref, () => ({
+      handleSetPath(image: string | null, imageType: string | null) {
+        if (!image) {
+          setPaths([])
+        } else {
+          setPaths([`data:image/${imageType};base64, ${image}`])
+        }
+      },
+      removePath() {
         setPaths([])
-      } else {
-        setPaths([`data:image/${imageType};base64, ${image}`])
+      },
+      restoreImage() {
+        setPaths(backupImage)
+        setBackupImage([])
+      },
+    }))
+
+    const [paths, setPaths] = useState<string[]>([])
+    const [backupImage, setBackupImage] = useState<string[]>([])
+
+    useEffect(() => {
+      return () => {
+        setPaths([])
+        setBackupImage([])
       }
-    },
-    removePath() {
-      setPaths([])
-    },
-    restoreImage() {
-      setPaths(backupImage)
-      setBackupImage([])
-    },
-  }))
+    }, [])
 
-  const [paths, setPaths] = useState<string[]>([])
-  const [backupImage, setBackupImage] = useState<string[]>([])
-
-  useEffect(() => {
-    return () => {
-      setPaths([])
-      setBackupImage([])
+    const handleUploadImage = (acceptedFiles: any) => {
+      getBase64(acceptedFiles[0], (result) => {
+        if (sendUploadImage) {
+          const imageType = acceptedFiles[0].type.replace('image/', '')
+          sendUploadImage(result, imageType)
+        }
+      })
+      setPaths(acceptedFiles.map((file: File) => URL.createObjectURL(file)))
     }
-  }, [])
 
-  const handleUploadImage = (acceptedFiles: any) => {
-    getBase64(acceptedFiles[0], (result) => {
-      if (sendUploadImage) {
-        const imageType = acceptedFiles[0].type.replace('image/', '')
-        sendUploadImage(result, imageType)
-      }
-    })
-    setPaths(acceptedFiles.map((file: File) => URL.createObjectURL(file)))
-  }
+    const handleChange = () => {
+      handleChangeImage?.()
+      setBackupImage(paths)
+      setPaths([])
+    }
 
-  const handleChange = () => {
-    if (from === 'profilePicture') {
+    const handleDelete = () => {
       handleResetImage?.()
+      setPaths([])
+      const closeDeleteLogoModalBtn = document.getElementById('closeDeleteLogoModalBtn')
+      closeDeleteLogoModalBtn?.click()
     }
-    handleChangeImage?.()
-    setBackupImage(paths)
-    setPaths([])
-  }
 
-  const handleDelete = () => {
-    handleResetImage?.()
-    setPaths([])
-    const closeDeleteLogoModalBtn = document.getElementById('closeDeleteLogoModalBtn')
-    closeDeleteLogoModalBtn?.click()
-  }
-
-  return (
-    <>
-      <Dropzone
-        noClick={false}
-        onDrop={(acceptedFiles) => handleUploadImage(acceptedFiles)}
-        multiple={from === 'patient-forms' ? true : false}
-        accept={acceptedFileFormats}
-      >
-        {({ getRootProps, getInputProps }) => (
-          <section className='drop-zone d-flex align-items-center justify-content-center text-center cursor-pointer'>
-            <div {...getRootProps()} className='w-100 h-100 d-flex align-items-center justify-content-center'>
-              <input {...getInputProps()} />
-              {paths.length ? (
-                paths.map((path) => <img className='w-100 h-100' key={path} src={path} />)
-              ) : (
-                <p className='mb-0 dropzone-text p-2'>
-                  {from === 'patient-forms' ? 'Upload one or more files (PDF, DOC, ...)' : 'Upload image (optional)'}
-                </p>
-              )}
-            </div>
-          </section>
-        )}
-      </Dropzone>
-      {!!paths.length && (
-        <div className='d-flex align-items-center justify-content-center mt-2'>
-          <button className='btn-with-border fw-bold border-0 bg-transparent' type='button' onClick={handleChange}>
-            <img src={Edit} className='me-2 edit-icon' />
-            <span className='light-text'>Change</span>
-          </button>
-          {from === 'businessLogo' && (
-            <button
-              className='btn-with-border fw-bold border-0 bg-transparent'
-              type='button'
-              data-bs-toggle='modal'
-              data-bs-target='#DeleteLogo'
-            >
-              <img src={Trash} className='me-2' />
-              <span className='danger-text'>Delete</span>
-            </button>
+    return (
+      <>
+        <Dropzone
+          noClick={false}
+          onDrop={(acceptedFiles) => handleUploadImage(acceptedFiles)}
+          multiple={from === 'patient-forms' ? true : false}
+          accept={acceptedFileFormats}
+        >
+          {({ getRootProps, getInputProps }) => (
+            <section className='drop-zone d-flex align-items-center justify-content-center text-center cursor-pointer'>
+              <div {...getRootProps()} className='w-100 h-100 d-flex align-items-center justify-content-center'>
+                <input {...getInputProps()} />
+                {paths.length ? (
+                  paths.map((path) => <img className='w-100 h-100' key={path} src={path} />)
+                ) : (
+                  <p className='mb-0 dropzone-text p-2'>
+                    {from === 'patient-forms' ? 'Upload one or more files (PDF, DOC, ...)' : 'Upload image (optional)'}
+                  </p>
+                )}
+              </div>
+            </section>
           )}
-        </div>
-      )}
+        </Dropzone>
+        {!!paths.length && (
+          <div className='d-flex align-items-center justify-content-center mt-2'>
+            <button className='btn-with-border fw-bold border-0 bg-transparent' type='button' onClick={handleChange}>
+              <img src={Edit} className='me-2 edit-icon' />
+              <span className='light-text'>Change</span>
+            </button>
+            {!!showDelete && (
+              <button
+                className='btn-with-border fw-bold border-0 bg-transparent'
+                type='button'
+                data-bs-toggle='modal'
+                data-bs-target='#DeleteLogo'
+              >
+                <img src={Trash} className='me-2' />
+                <span className='danger-text'>Delete</span>
+              </button>
+            )}
+          </div>
+        )}
 
-      <DeleteLogo handleDelete={handleDelete} />
-    </>
-  )
-})
+        <DeleteLogo handleDelete={handleDelete} />
+      </>
+    )
+  },
+)
 
 export default ImageDropZone
