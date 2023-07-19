464e9a18ca87c76bcc45caa51d2aba1b8125bf43
Navjot Kaur <navjot.kaur@asiasoftwaresolutions.com>
2023-07-19 11:02:24 +0530

    Changed delete logo text to delete photo

--- src/components/modals/DeleteLogo.tsx
+++ src/components/modals/DeleteLogo.tsx
@@ -11,7 +11,7 @@ const DeleteLogo = ({ handleDelete }: Props) => {
       <div className='modal-dialog modal-dialog-centered w-481'>
         <div className='modal-content p-40'>
           <div className='modal-header border-0 p-0 align-items-center mb-40'>
-            <h2 className='main-h mb-0'>Delete Logo?</h2>
+            <h2 className='main-h mb-0'>Delete Photo?</h2>
 
             <button
               type='button'
